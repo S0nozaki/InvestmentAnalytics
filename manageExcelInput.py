8e937dc9661511d91fb688f829102f564f9d866d
Max Roger Morillo Alvarado <maxrogermorillo@gmail.com>
2021-05-31 02:52:47 -0300

    Added support for multiple input files with selectable currency per file

--- manageExcelInput.py
+++ manageExcelInput.py
@@ -4,36 +4,34 @@ import pandas as pd
 
 with open("config.yml", "r") as file_descriptor:
     config = yaml.safe_load(file_descriptor)
-    sourceExcelFile = config["file"]
     db = config["db"]
-    table = config["table"]
-    columnsSelectedForDBList = config["columnsSelectedForDBList"]
+    table = config["transaction_table"]
+    columns_selected_for_db = config["columns_selected_for_db"]
     ticker = config["ticker"]
     currency = config["currency"]
-    ARS = config["ARS"]
+    currencies = config["currencies"]
+    files_and_currencies = config["files_and_currencies"]
 
-def open_file_connection():
-    xls = pd.read_excel(sourceExcelFile)
-    return xls
 
-def drop_empty_rows(dataframe, columnToFilter):
-    return dataframe.dropna(subset=[columnToFilter])
+def open_file_connection(path):
+    xls_dataframe = pd.read_excel(path)
+    return xls_dataframe
+
+
+def drop_empty_rows(dataframe, column_to_filter):
+    return dataframe.dropna(subset=[column_to_filter])
+
 
 def create_dataframe():
-    file = open_file_connection()
-    file[currency] = ARS
-    return drop_empty_rows(file[columnsSelectedForDBList],ticker)
+    dataframe = pd.DataFrame()
+    for filepath, file_currency in files_and_currencies.items():
+        file_dataframe = open_file_connection(filepath)
+        file_dataframe[currency] = currencies[file_currency]
+        dataframe = dataframe.append(file_dataframe)
+    return drop_empty_rows(dataframe[columns_selected_for_db], ticker)
 
-def create_db():
+
+def create_transaction_table():
     conn = sql.connect(db)
     create_dataframe().reset_index(drop=True).to_sql(table, conn)
-    conn.close()
-
-def read_from_db():
-    conn = sql.connect(db)
-    c = conn.cursor()
-    c.execute("SELECT * FROM " + table)
-    for row in c.fetchall():
-        print(row)
-    c.close()
     conn.close()
