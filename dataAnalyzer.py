20603e3db9fb3dac2cb80bb0e6a640e1fe0c9567
Max Roger Morillo Alvarado <maxrogermorillo@gmail.com>
2021-06-01 03:21:16 -0300

    Added analysis exclusion feature

--- dataAnalyzer.py
+++ dataAnalyzer.py
@@ -8,17 +8,26 @@ locale.setlocale(locale.LC_ALL, '')
 with open("config.yml", "r") as file_descriptor:
     config = yaml.safe_load(file_descriptor)
     db = config["db"]
-    table = config["table"]
+    table = config["transaction_table"]
+    excluded_tickers = config["excluded_tickers"]
     date = config["date"]
     quantity = config["quantity"]
     ticker = config["ticker"]
     value = config["value"]
 
+
 def get_dataframe():
     conn = sql.connect(db)
-    return pd.read_sql_query("SELECT * FROM " + table, conn, parse_dates = {date:'%d/%m/%y'})
+    df = pd.read_sql_query("SELECT * FROM '" + table +
+                           "'", conn, parse_dates={date: '%d/%m/%y'})
+    return filter_column(df, ticker, excluded_tickers)
 
-def ticker_balance(selected_ticker):
+
+def filter_column(dataframe, column, filter):
+    return dataframe[dataframe[column].isin(filter) == False]
+
+
+def get_ticker_balance(selected_ticker):
     file = get_dataframe()
     transactions = file.loc[file[ticker] == selected_ticker]
     position = 0
@@ -35,11 +44,12 @@ def ticker_balance(selected_ticker):
             result += transaction_value
             transaction_value = 0
             unfinished_transaction = 0
-
     return [result, position]
 
-def total_balance():
+
+def get_valid_tickers():
     return get_dataframe()[ticker].unique()
+
 
 def filter_by_year(year):
     file = get_dataframe()
