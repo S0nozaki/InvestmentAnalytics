0f8c2b3b7cf37d7cd50e86d7abc51bd244ab9be2
Max Roger Morillo Alvarado <maxrogermorillo@gmail.com>
2021-06-06 09:18:20 -0300

    Added support for analysis with local and reference currencies

--- dataAnalyzer.py
+++ dataAnalyzer.py
@@ -2,55 +2,120 @@ import pandas as pd
 import sqlite3 as sql
 import locale
 import yaml
+from decimal import Decimal
 
 locale.setlocale(locale.LC_ALL, '')
 
 with open("config.yml", "r") as file_descriptor:
     config = yaml.safe_load(file_descriptor)
     db = config["db"]
-    table = config["transaction_table"]
+    transaction_table = config["transaction_table"]
+    exchange_table = config["dolar_table"]
     excluded_tickers = config["excluded_tickers"]
     date = config["date"]
     quantity = config["quantity"]
     ticker = config["ticker"]
+    price = config["price"]
     value = config["value"]
+    currency = config["currency"]
+    local_currency = config["local_currency"]
+    reference_currency = config["reference_currency"]
 
 
-def get_dataframe():
+def get_dataframe(table):
     conn = sql.connect(db)
-    df = pd.read_sql_query("SELECT * FROM '" + table +
-                           "'", conn, parse_dates={date: '%d/%m/%y'})
-    return filter_column(df, ticker, excluded_tickers)
+    df = pd.read_sql_query("SELECT * FROM '" + table["Name"] +
+                           "'", conn, parse_dates={table["Date"]: '%d/%m/%y'})
+    conn.close()
+    return df
 
 
 def filter_column(dataframe, column, filter):
     return dataframe[dataframe[column].isin(filter) == False]
 
 
+def get_currencies(dataframe):
+    return dataframe[currency].unique()
+
+
+def get_price_per_currency(transaction, curr):
+    exchange_rate = Decimal(str(transaction[exchange_table["Rate"]]))
+    transaction_price = Decimal(str(transaction[price]))
+    if(transaction[currency] == reference_currency) and (curr == local_currency):
+        return str(transaction_price * exchange_rate)
+    elif (transaction[currency] == local_currency) and (curr == reference_currency):
+        return str(transaction_price / exchange_rate)
+    return str(transaction_price)
+
+
 def get_ticker_balance(selected_ticker):
-    file = get_dataframe()
+    file = filter_column(get_dataframe(transaction_table),
+                         ticker, excluded_tickers)
+    exchange_rate = get_exchange_dataframe()
     transactions = file.loc[file[ticker] == selected_ticker]
-    position = 0
-    transaction_value = locale.atof('0,00')
-    result = 0
-    unfinished_transaction = 0
-
+    transactions = transactions.merge(exchange_rate)
+    currencies = get_currencies(transactions)
+    for curr in currencies:
+        transactions[curr] = transactions.apply(
+            lambda x: get_price_per_currency(x, curr), axis=1)
+    result = dict.fromkeys(currencies, 0)
+    positions = []
+    total_position = 0
     for index, transaction in transactions.iterrows():
-        if unfinished_transaction == 0:
-            unfinished_transaction = 1
-        position += int(locale.atof(transaction[quantity]))
-        transaction_value += locale.atof(transaction[value])
-        if position <= 0 and unfinished_transaction != 0:
-            result += transaction_value
-            transaction_value = 0
-            unfinished_transaction = 0
-    return [result, position]
+        quantity_to_process = transaction[quantity]
+        while quantity_to_process != 0:
+            # if there are no existing positions or if you can't close any operation
+            if (not positions) or ((positions[0][quantity] > 0) == (transaction[quantity] > 0)):
+                positions.append(create_position(
+                    transaction[quantity], transaction[currencies]))
+                total_position += quantity_to_process
+                quantity_to_process = 0
+                continue
+            # if the transaction to process can't be closed completely by the oldest position
+            if ((positions[0][quantity] > 0) != (positions[0][quantity] + quantity_to_process >= 0)):
+                for curr in currencies:
+                    result[curr] += abs(positions[0][quantity]) * \
+                        (Decimal(transaction[curr]) -
+                         Decimal(positions[0][curr]))
+                quantity_to_process += positions[0][quantity]
+                total_position -= positions[0][quantity]
+                positions.pop(0)
+            else:  # if the transaction can be closed completely
+                for curr in currencies:
+                    result[curr] += abs(quantity_to_process) * \
+                        (Decimal(transaction[curr]) -
+                         Decimal(positions[0][curr]))
+
+                positions[0][quantity] += quantity_to_process
+                if positions[0][quantity] == 0:
+                    positions.pop(0)
+                total_position += quantity_to_process
+                quantity_to_process = 0
+    return [result, total_position]
 
 
-def get_valid_tickers():
-    return get_dataframe()[ticker].unique()
+def create_position(quantity, currencies):
+    position = {"Cantidad": quantity}
+    for position_currency, currency_price in currencies.iteritems():
+        position[position_currency] = currency_price
+    return position
+
+
+def get_exchange_dataframe():
+    df = get_dataframe(exchange_table)
+    df = df.rename(columns={exchange_table["Date"]: transaction_table["Date"]})
+    return df[[transaction_table["Date"], exchange_table["Rate"]]]
+
+
+def get_total_metadata():
+    total = filter_column(get_dataframe(
+        transaction_table), ticker, excluded_tickers)
+    tickers = total[ticker].unique()
+    currencies_counter = dict.fromkeys(get_currencies(total), 0)
+    return [tickers, currencies_counter]
 
 
 def filter_by_year(year):
-    file = get_dataframe()
+    file = filter_column(get_dataframe(transaction_table),
+                         ticker, excluded_tickers)
     return file.loc[file[date].dt.year == year].reset_index(drop=True)
--- dataVisualizer.py
+++ dataVisualizer.py
@@ -1,17 +1,26 @@
 import dataAnalyzer
 
+
 def filter_by_year(year):
     print(dataAnalyzer.filter_by_year(year))
 
+
 def ticker_balance(selected_ticker):
-    result, remaining = dataAnalyzer.ticker_balance(selected_ticker)
-    print('Results of ' + selected_ticker + ': ' + str(result))
+    result, remaining = dataAnalyzer.get_ticker_balance(selected_ticker)
+    for entry in result.keys():
+        print('Results of ' + selected_ticker +
+              ' in ' + entry + ": " + str(result[entry]))
     if remaining > 0:
         print('Existing position: ' + str(remaining))
     return result
 
+
 def total_balance():
-    sum = 0
-    for ticker in dataAnalyzer.total_balance():
-        sum += ticker_balance(ticker)
-    print("Total earnings: " + str(sum))
+    tickers, currencies_counter = dataAnalyzer.get_total_metadata()
+    for ticker in tickers:
+        result = ticker_balance(ticker)
+        for currency_entry in result.keys():
+            currencies_counter[currency_entry] += result[currency_entry]
+    for currency in currencies_counter.keys():
+        print("Total earnings in " + currency +
+              ": " + str(currencies_counter[currency]))
